a851663a643b9abea94eed35c73f056f65a00ee0
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-09 22:39:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function (req, res) {
 res.send('Article One will be served here');
-));
+});
 
 app.get('/article-two', function (req, res) {
 res.send('Article Two will be served here');
-));
+});
 
 app.get('/article-three', function (req, res) {
 res.send('Article Three will be served here');
-));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
