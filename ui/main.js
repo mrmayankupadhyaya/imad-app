a65345afc2b7be4c832bb641aaba972933dd438b
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-15 20:21:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,10 @@
-console.log('Loaded!');
-// change the text of mail div
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-
-// move image
-var img = document.getElementById('madi');
-function moveRight () {
-  marginLeft = marginLeft + 1;
-  img.style.marginLeft = marginLeft + 'px';
-}
-var marginLeft = 0;
-img.onclick = function () {
-  var interval = setInterval(moveRight, 50);
-  img.style.marginLeft = '100px';  
-    
+// counter
+var button = document.getElementByID('counter');
+var counter = 0;
+button.onclick = function () {
+  
+  // render counter
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
