2357ac61eeaac6fec236b4a7ca9c525543d04bfc
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-15 18:45:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 // change the text of mail div
-var element = document.getelementById('main-text');
+var element = document.getElementById('main-text');
 element.innerHtml = 'New Value';
 
