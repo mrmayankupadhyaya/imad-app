aadf431779a50595289fcdfbed84274bde74174c
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-20 20:49:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ function createTemplate (data) {
     var heading = data.heading;
     var date = data.date;
     var content = data.content;
-    
+
     var htmlTemplate = `
     <html>
         <head>
