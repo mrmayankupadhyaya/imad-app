725abc73dfeea1d1b739bbc6aa968fcc5539a160
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-20 22:32:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -174,9 +174,9 @@ app.get('/submit-name', function (req, res) { // URL: /submit-name?name=xxxxx
 });
 
 app.get('/articles/:articleName', function (req, res) {
-  
-  pool.query("SELECT * FROM article_db WHERE title = '" + req.params.articleName + "'", function (err, result) {
-//   pool.query("SELECT * FROM article_db WHERE title = $1", [req.params.articleName], function (err, result) {
+  // http://mrmayankupadhyaya.imad.hasura-app.io/articles/'; DELETE FROM article where 'a' = 'a
+//   pool.query("SELECT * FROM article_db WHERE title = '" + req.params.articleName + "'", function (err, result) {
+  pool.query("SELECT * FROM article_db WHERE title = $1", [req.params.articleName], function (err, result) {
      if (err) {
          res.status(500).send(err.toString());
      } else {
