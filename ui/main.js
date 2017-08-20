2307f64d87fef709bb450465024adc378ca472fa
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-20 20:27:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,6 +23,39 @@ request.send(null);
 
 };
 
+// submit comment
+var comment = document.getElementById('comment_btn');
+comment.onclick = function () {
+  
+  // make req to server and send the comment
+// create a request object
+var request = new XMLHttpRequest();
+
+// capture the response and store in variable
+request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+  // capture a list of comment and render as list
+  var comments = request.responseText;
+  names = JSON.parse(comments);
+  var list_c = '';
+  for (var i=0; i< comments.length; i++) {
+    list_c += '<li>' + comments[i] + '</li>';  
+  }
+  var ul = document.getElementById('commentlist');
+  ul.innerHTML = list_c;
+        }
+    }
+};
+
+var commentInput = document.getElementById('comment');
+var comment = commentInput.value;
+// make the request
+request.open('GET', 'http://mrmayankupadhyaya.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+request.send(null);
+  
+    
+};
 // submit name
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
