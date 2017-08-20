46a2f0e6dceb7a9682b46e32e8d6eb76c1ce6223
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-20 22:16:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,48 +14,48 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    'article-one': {
-    title: 'Article One | Mayank',
-    heading: 'Article One',
-    date: '15 Aug, 2017',
-    content: `
+// var articles = {
+//     'article-one': {
+//     title: 'Article One | Mayank',
+//     heading: 'Article One',
+//     date: '15 Aug, 2017',
+//     content: `
     
-                <p>
-                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-                </p>
-                <p>
-                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-                </p>
-                <p>
-                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-                </p>
+//                 <p>
+//                     This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+//                 </p>
+//                 <p>
+//                     This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+//                 </p>
+//                 <p>
+//                     This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+//                 </p>
     
-    `,
-},
-    'article-two': {
-            title: 'Article Two | Mayank',
-    heading: 'Article Two',
-    date: '16 Aug, 2017',
-    content: `
+//     `,
+// },
+//     'article-two': {
+//             title: 'Article Two | Mayank',
+//     heading: 'Article Two',
+//     date: '16 Aug, 2017',
+//     content: `
     
-                <p>
-                    This is the content for my second article.
-                </p>
-    `,
-    },
-    'article-three': {
-                    title: 'Article Three | Mayank',
-    heading: 'Article Three',
-    date: '17 Aug, 2017',
-    content: `
+//                 <p>
+//                     This is the content for my second article.
+//                 </p>
+//     `,
+//     },
+//     'article-three': {
+//                     title: 'Article Three | Mayank',
+//     heading: 'Article Three',
+//     date: '17 Aug, 2017',
+//     content: `
     
-                <p>
-                    This is the content for my third article.
-                </p>
-    `,
-    }
-};
+//                 <p>
+//                     This is the content for my third article.
+//                 </p>
+//     `,
+//     }
+// };
 
 function createTemplate (data) {
     var title = data.title;
