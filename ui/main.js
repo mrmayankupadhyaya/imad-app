0eff289399da2a28c81670eab42096a1d0d16707
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-20 21:07:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,39 +23,39 @@ request.send(null);
 
 };
 
-// submit comment
-var submit_comment = document.getElementById('comment_btn');
-submit_comment.onclick = function () {
+// // submit comment
+// var submit_comment = document.getElementById('comment_btn');
+// submit_comment.onclick = function () {
   
-  // make req to server and send the comment
-// create a request object
-var request = new XMLHttpRequest();
+//   // make req to server and send the comment
+// // create a request object
+// var request = new XMLHttpRequest();
 
-// capture the response and store in variable
-request.onreadystatechange = function () {
-    if (request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-  // capture a list of comment and render as list
-  var comments = request.responseText;
-  comments = JSON.parse(comments);
-  var list = '';
-  for (var i=0; i< comments.length; i++) {
-    list += '<li>' + comments[i] + '</li>';  
-  }
-  var ul = document.getElementById('commentlist');
-  ul.innerHTML = list;
-        }
-    }
-};
+// // capture the response and store in variable
+// request.onreadystatechange = function () {
+//     if (request.readyState === XMLHttpRequest.DONE) {
+//         if (request.status === 200) {
+//   // capture a list of comment and render as list
+//   var comments = request.responseText;
+//   comments = JSON.parse(comments);
+//   var list = '';
+//   for (var i=0; i< comments.length; i++) {
+//     list += '<li>' + comments[i] + '</li>';  
+//   }
+//   var ul = document.getElementById('commentlist');
+//   ul.innerHTML = list;
+//         }
+//     }
+// };
 
-var commentInput = document.getElementById('comment');
-var comment = commentInput.value;
-// make the request
-request.open('GET', 'http://mrmayankupadhyaya.imad.hasura-app.io/submit-comment?comment=' + comment, true);
-request.send(null);
+// var commentInput = document.getElementById('comment');
+// var comment = commentInput.value;
+// // make the request
+// request.open('GET', 'http://mrmayankupadhyaya.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+// request.send(null);
   
     
-};
+// };
 
 // submit name
 var submit = document.getElementById('submit_btn');
