2be495d7feca74fe97d414efb75a3f7f8600c535
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-20 20:00:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,22 +10,15 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
-  // capture a list of names and render as list
-  var names = request.responseText;
-  names = JSON.parse(names);
-  var list = '';
-  for (var i=0; i< names.length; i++) {
-    list += '<li>' + names[i] + '</li>';  
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
-
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
         }
     }
 };
 
 // make the request
-request.open('GET', 'http://mrmayankupadhyaya.imad.hasura-app.io/submit-name?name=' + name, true);
+request.open('GET', 'http://mrmayankupadhyaya.imad.hasura-app.io/counter', true);
 request.send(null);
 
 };
@@ -38,5 +31,13 @@ submit.onclick = function () {
   
   // make req to server and send the name
   
+  // capture a list of names and render as list
+  var names = ['name1', 'name2', 'name3', 'name4'];
+  var list = '';
+  for (var i=0; i< names.length; i++) {
+    list += '<li>' + names[i] + '</li>';  
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
     
 };
