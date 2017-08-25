72b53bf76bed10d0518dfc8f0f081cec8b737c3a
mrmayankupadhyaya <30800809+mrmayankupadhyaya@users.noreply.github.com>
2017-08-25 16:55:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,27 @@
-// counter
-var button = document.getElementById('counter');
+// // counter
+// var button = document.getElementById('counter');
 
-button.onclick = function () {
+// button.onclick = function () {
     
-// create a request object
-var request = new XMLHttpRequest();
+// // create a request object
+// var request = new XMLHttpRequest();
 
-// capture the response and store in variable
-request.onreadystatechange = function () {
-    if (request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-};
+// // capture the response and store in variable
+// request.onreadystatechange = function () {
+//     if (request.readyState === XMLHttpRequest.DONE) {
+//         if (request.status === 200) {
+//             var counter = request.responseText;
+//             var span = document.getElementById('count');
+//             span.innerHTML = counter.toString();
+//         }
+//     }
+// };
 
-// make the request
-request.open('GET', 'http://mrmayankupadhyaya.imad.hasura-app.io/counter', true);
-request.send(null);
+// // make the request
+// request.open('GET', 'http://mrmayankupadhyaya.imad.hasura-app.io/counter', true);
+// request.send(null);
 
-};
+// };
 
 // // submit comment
 // var submit_comment = document.getElementById('comment_btn');
@@ -57,7 +57,7 @@ request.send(null);
     
 // };
 
-// submit name
+// submit username password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
   
@@ -69,24 +69,25 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
-  // capture a list of names and render as list
-  var names = request.responseText;
-  names = JSON.parse(names);
-  var list = '';
-  for (var i=0; i< names.length; i++) {
-    list += '<li>' + names[i] + '</li>';  
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+        console.log('user logged in');
+        alert('Logged in successfully');
+        } else if (request.status === 403) {
+            alert('Username/password is incorrect');
+        } else if (request.status === 500) {
+            alert('something went wrong on the server');
         }
     }
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+
 // make the request
-request.open('GET', 'http://mrmayankupadhyaya.imad.hasura-app.io/submit-name?name=' + name, true);
-request.send(null);
+request.open('POST', 'http://mrmayankupadhyaya.imad.hasura-app.io/login', true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));
   
     
 };
